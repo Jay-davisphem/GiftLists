b8108e11fad3ca733e2d31919b4bcd334f58deea
David Oluwafemi <davidoluwafemi178@gmail.com>
2022-12-28 12:32:08 +0100

    Chore: commit -m Week 3 project alchemmy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+
+const MerkleTree = require("../utils/MerkleTree");
+const niceList = require("../utils/niceList");
+
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,19 +12,23 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+//
+const merkleTree = new MerkleTree(niceList);
+const MERKLE_ROOT = merkleTree.getRoot();
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // TODO: prove that a name is in the list
+  const isInTheList = niceList.findIndex((n) => n === body.name);
+  console.log(body.name, 'tried to get a gift!');
+  if (isInTheList !== -1) {
     res.send("You got a toy robot!");
-  }
-  else {
+    console.log('He/She got it.')
+  } else {
     res.send("You are not on the list :(");
+    console.log('Your gifts are intact.')
   }
 });
 
